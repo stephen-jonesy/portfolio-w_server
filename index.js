fe59f1ee728aaebfe012a9d168c75161bfd6d5bd
Stephen Jones <stephen@stephens-air.home>
2021-10-01 19:17:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const express = require("express");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
+
+// instantiate an express app
+const app = express();
+
+//make the contact page the the first page on the app
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.send('what???', 404);
+});
+
+//port will be 5000 for testing
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+const transporter = nodemailer.createTransport( {
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  }
+});
+
+transporter.verify(function (error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+});
+
+app.post("/send", (req, res) => {
+    //1.
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+      console.log(fields);
+      Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+      });
+      console.log(data)
+  
+      //2. You can configure the object however you want
+      const mail = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: data.subject,
+        text: `${data.name} <${data.email}> \n${data.message}`,
+      };
+  
+      //3.
+      transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send(
+            {
+              ok: false,
+              data: 'Opps! Something went wrong'
+            });
+        } else {
+          res.status(200)
+          return res.status(200).send({
+            ok: true,
+            data: 'Success! Your message was sent'
+          });
+          
+        }
+      });
+
+    });
+});
+
---
+++ public/app.js
@@ -0,0 +1,40 @@
+const portfolioTiles = document.querySelectorAll('.portfolio-tiles');
+tilesImage = document.querySelectorAll('.tiles-img');
+const tilesText = document.querySelectorAll('.tiles-text-container');
+const burgerNav = document.querySelector('.nav-burger');
+
+console.log(window.innerWidth)
+
+const toggleNav = () => {
+    const header = document.querySelector('.header');
+    header.classList.toggle('side-nav');
+}
+
+/*
+function hoverOver(index) {
+    console.log('hovered');
+    tilesImage[index].classList.add('tiles-img-active');
+    tilesText[index].classList.add('tiles-text-container-active');
+}
+
+function hoverLeave(index) {
+    console.log('off')
+    tilesImage[index].classList.remove('tiles-img-active');
+    tilesText[index].classList.remove('tiles-text-container-active');
+
+}
+
+portfolioTiles.forEach((element, index) => {
+    element.addEventListener('mouseover',() => hoverOver(index));
+    element.addEventListener('mouseleave', () => hoverLeave(index));
+});
+*/
+
+document.addEventListener('click', (e) => {
+    console.log(e.target)
+    if(e.target.matches('.fas')) {
+        toggleNav()
+        
+
+    }
+})
